05936952f5855d52546b395c88952ef3a7f16a89
Novout <novout@hotmail.com>
2026-04-24 21:04:38 -0300

    feat: description in hand card

--- src/components/cards/GameCardHand.vue
+++ src/components/cards/GameCardHand.vue
@@ -1,5 +1,6 @@
 <template>
   <div
+    ref="card"
     :style="{
       backgroundColor: content?.backgroundColor,
       color: content?.textColor,
@@ -15,15 +16,21 @@
       <GameIcons :icon="props.content?.icon" />
     </div>
     <p>{{ props.content?.name }}</p>
+    <p v-if="isHovered" class="text-xs">{{ props.content?.description }}</p>
   </div>
 </template>
 
 <script setup lang="ts">
+  import { useTemplateRef } from 'vue'
   import type { Card, Maybe } from '../../types'
+  import { useElementHover } from '@vueuse/core'
 
   const props = defineProps<{
     content: Maybe<Card>
   }>()
 
   const emit = defineEmits(['select'])
+
+  const myHoverableElement = useTemplateRef('card')
+  const isHovered = useElementHover(myHoverableElement)
 </script>
