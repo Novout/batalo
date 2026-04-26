c6f5a03edf1e93a32d6a2c8117631a5ac5d2b612
Novout <novout@hotmail.com>
2026-04-25 22:43:41 -0300

    feat: pwa options

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'pathe'
+import fg from 'fast-glob'
 import UnoCSS from 'unocss/vite'
 import Components from 'unplugin-vue-components/vite'
 import vue from '@vitejs/plugin-vue'
@@ -12,6 +14,54 @@ export default defineConfig({
     UnoCSS(),
     Components({ dts: true }),
     version(),
-    VitePWA({ registerType: 'autoUpdate' }),
+    VitePWA({
+        base: '/',
+        registerType: 'prompt',
+        includeAssets: fg.sync('**/*.{png,svg,json,mp3,ico,txt,xml,ttf}', {
+          cwd: resolve(__dirname, 'public'),
+        }),
+        manifest: {
+          name: 'Batalo',
+          short_name: 'Batalo',
+          description: 'A Frost Remnants Card Game.',
+          theme_color: '#1F2937',
+          categories: ['game', 'web', 'card'],
+        },
+        workbox: {
+          maximumFileSizeToCacheInBytes: 3145728000,
+          sourcemap: false,
+          globPatterns: ['**/*.{css,js,html,ico,txt,woff2,ttf,png,svg,json}'],
+          runtimeCaching: [
+            {
+              urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'google-fonts-cache',
+                expiration: {
+                  maxEntries: 10,
+                  maxAgeSeconds: 60 * 60 * 24 * 365,
+                },
+                cacheableResponse: {
+                  statuses: [0, 200],
+                },
+              },
+            },
+            {
+              urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'gstatic-fonts-cache',
+                expiration: {
+                  maxEntries: 10,
+                  maxAgeSeconds: 60 * 60 * 24 * 365,
+                },
+                cacheableResponse: {
+                  statuses: [0, 200],
+                },
+              },
+            },
+          ],
+        },
+      }),
   ],
 })
